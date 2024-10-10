ffdbb4c3b78b80a7305f9817bdb0717c101d6383
pedro1826 <pedrotolentxd3@gmail.com>
2024-10-10 12:45:41 -0500

    Add files via upload

---
+++ actividad 8.cpp
@@ -0,0 +1,26 @@
+#include "iostream"
+using namespace std;
+
+int main()
+{
+	//problema 1.4 construya un diagrama de flujo que resuelva el problema que tienen en una 
+	//gasolinera. Los surtidores de la misma regisran lo que "suerten"en galones, ero elprecio de la gasolina 
+	//este fijado en "litros". El programa debe calcular e imprimir lo que hay que cobrarle al cliente.
+	//cada galon tiene  3.785 litros, y el precio es 8.20
+	//declaracion de variables 
+	
+	float GALONES, TOTAL;
+	const float GALON=3.785, PRECIOLITRO=8.20;
+	
+	//Entrada de datos 
+	
+	cout<<" Escribe cantidad de GALONES comprados "<<"\n";
+	cin>> GALONES;
+	
+	//CALCULO
+	TOTAL= GALONES*GALON*PRECIOLITRO;
+	
+	//SE IMPRIME RESULTADOS 
+	cout<<"Hay que cobrar al cliente por  "<<GALONES<<" galones  "<<"debe pagar  "<<TOTAL<<"  pesos  "<"\n";
+	return 0;
+}
